f10ce43c1e3e1a61413b7cb0e286dd3af58b2266
SharmaTechGroup <147160462+SharmaTechGroup@users.noreply.github.com>
2026-06-19 20:56:05 +0530

    Modifeid Form

--- src/components/form-demo/form-demo.jsx
+++ src/components/form-demo/form-demo.jsx
@@ -1,39 +1,84 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
+import { useFormik } from "formik"
 
 
 export function FormDemo(){
 
-   
-    function handleSubmit(e){
-        e.preventDefault();
-        let data = new FormData(e.target)
-        console.log({UserName:data.get('UserName'), Age:parseInt(data.get('Age')), City:data.get('City'), Gender: data.get('Gender')});
+    function ValidateUser(user){
+        let errors = {};
+
+        if(user.UserName.length===0){
+            errors.UserName = "User Name Required";
+        } else {
+            if(user.UserName.length<4){
+                errors.UserName = "Name too short";
+            }
+        }
+
+        if(user.Age===0){
+            errors.Age = "Age Required";
+        } else {
+            if(isNaN(user.Age)){
+                errors.Age = "Age must be a number";
+            }
+        }
+
+        if(user.City===''){
+            errors.City = "City Required";
+        } 
+
+        if(user.Gender===''){
+            errors.Gender = "Gender Required";
+        }
+
+
+        return errors;
     }
+    
+    const formik = useFormik({
+         initialValues: {
+            UserName:'',
+            Age:0,
+            City:'',
+            Gender:''
+         },
+         validate: ValidateUser,
+         onSubmit: (user)=>{
+            console.log(user);
+         }
+    })
+
 
     return(
         <div className="container-fluid">
             <h2>Register User</h2>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={formik.handleSubmit}>
                 <dl>
                     <dt>User Name</dt>
-                    <dd><input type="text" name="UserName"  /></dd>
+                    <dd><input type="text" onBlur={formik.handleBlur} onChange={formik.handleChange} name="UserName"  /></dd>
+                    <dd className="text-danger">{ formik.touched.UserName && formik.errors.UserName}</dd>
                     <dt>Age</dt>
-                    <dd><input type="number" name="Age"  /></dd>
+                    <dd><input type="number" onBlur={formik.handleBlur} onChange={formik.handleChange}  name="Age"  /></dd>
+                    <dd className="text-danger">{formik.touched.Age && formik.errors.Age}</dd>
                     <dt>City</dt>
                     <dd>
-                        <select name="City">
-                            <option>Select City</option>
+                        <select name="City" onBlur={formik.handleBlur} onChange={formik.handleChange} >
+                            <option value=''>Select City</option>
                             <option>Delhi</option>
                             <option>Hyd</option>
                         </select>
                     </dd>
+                    <dd className="text-danger">{formik.touched.City && formik.errors.City}</dd>
                     <dt>Gender</dt>
                     <dd>
-                        <input type="radio" name="Gender" value="Male" / > Male
-                        <input type="radio" name="Gender" value="Female" / > Female
+                        <input type="radio" onBlur={formik.handleBlur} onChange={formik.handleChange}  name="Gender" value="Male" / > Male
+                        <input type="radio" onBlur={formik.handleBlur} onChange={formik.handleChange} name="Gender" value="Female" / > Female
                     </dd>
+                    <dd className="text-danger">{formik.touched.Gender && formik.errors.Gender}</dd>
                 </dl>
-                <button>Submit</button>
+                <button type="submit" disabled={(formik.isValid)?false:true} >Submit</button>
+                <button className={(formik.dirty)?'d-inline':'d-none'} >Save</button>
+                
             </form>
         </div>
     )
